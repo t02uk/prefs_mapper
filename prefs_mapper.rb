7e6444c8814723f9e335f8ca0fa4741c86a57332
t02uk <tetsuya@pc4.(none)>
2012-05-05 20:31:01 +0900

    fix indent

--- prefs_mapper.rb
+++ prefs_mapper.rb
@@ -4,164 +4,164 @@ require 'erb'
 
 class PrefsMapper
 
-	DEFAULT_FILE = "#{File.expand_path(File.dirname(__FILE__))}/default.conf"
-	TEMPLATE_FILE_NAME = "templ.scala"
+  DEFAULT_FILE = "#{File.expand_path(File.dirname(__FILE__))}/default.conf"
+  TEMPLATE_FILE_NAME = "templ.scala"
 
-	def initialize(source)
-		@xml_source = source
-	end
+  def initialize(source)
+    @xml_source = source
+  end
 
-	def confirm()
+  def confirm()
 
-		# load default setting
-		defaultSetting = {}
-		if File.exist?(DEFAULT_FILE)
-			defaultSetting = eval(File.read(DEFAULT_FILE))
-		else 
-			defaultSetting = {
-				namespace: "",
-				file_name: ""
-			}
-		end
+    # load default setting
+    defaultSetting = {}
+    if File.exist?(DEFAULT_FILE)
+      defaultSetting = eval(File.read(DEFAULT_FILE))
+    else 
+      defaultSetting = {
+        namespace: "",
+        file_name: ""
+      }
+    end
 
-		print "input loader namespace (#{defaultSetting[:namespace]})> "
-		@namespace = STDIN.gets.chomp
-		@namespace = defaultSetting[:namespace] if @namespace =~ /^$/
-		defaultSetting[:namespace] = @namespace
+    print "input loader namespace (#{defaultSetting[:namespace]})> "
+    @namespace = STDIN.gets.chomp
+    @namespace = defaultSetting[:namespace] if @namespace =~ /^$/
+    defaultSetting[:namespace] = @namespace
 
-		print "input loader filename (#{defaultSetting[:file_name]})> "
-		@file_name = STDIN.gets.chomp
-		@file_name = defaultSetting[:file_name] if @file_name =~ /^$/
-		defaultSetting[:file_name] = @file_name
+    print "input loader filename (#{defaultSetting[:file_name]})> "
+    @file_name = STDIN.gets.chomp
+    @file_name = defaultSetting[:file_name] if @file_name =~ /^$/
+    defaultSetting[:file_name] = @file_name
 
-		# save default setting
-		File.open(DEFAULT_FILE, 'w') do |f|
-			f.puts defaultSetting.inspect
-		end
+    # save default setting
+    File.open(DEFAULT_FILE, 'w') do |f|
+      f.puts defaultSetting.inspect
+    end
 
-		raise "failed to get loader base name " unless @file_name =~ /^(.+)\.scala/
-		@class_name = $1
-	end
+    raise "failed to get loader base name " unless @file_name =~ /^(.+)\.scala/
+    @class_name = $1
+  end
 
-	class Item
-		include Enumerable
+  class Item
+    include Enumerable
 
-		# unused
-		def each
-			@children.each do |e|
-				yield e
-			end
-		end
+    # unused
+    def each
+      @children.each do |e|
+        yield e
+      end
+    end
 
-		def has_child?
-			not @children.empty?
-		end
+    def has_child?
+      not @children.empty?
+    end
 
-		attr_reader :component, :name, :title, :default, :type, :load_method, :children
+    attr_reader :component, :name, :title, :default, :type, :load_method, :children
 
-		def initialize(e)
-			@component = e.name
-			@name = e.attributes['android:key']
-			@title = e.attributes['android:title']
-			@default = e.attributes['android:defaultValue']
-			@type = e.attributes['my:type']
+    def initialize(e)
+      @component = e.name
+      @name = e.attributes['android:key']
+      @title = e.attributes['android:title']
+      @default = e.attributes['android:defaultValue']
+      @type = e.attributes['my:type']
 
-			# inspect type from default value
-			unless @type
-				if @default
-					@type = if @default.downcase == "true" || @default.downcase == "false"
-						"Boolean"
-					elsif @default =~ /^-?\d*\.\d+(f|F)?$/
-						"Float"
-					elsif @default =~ /^-?\d+(l)$/
-						"Long"
-					elsif @default =~ /^-?\d+$/
-						"Int"
-					else
-						"String"
-					end
-				end
-			end
-			# default
-			@type ||= "String"
-			
-			# get load_method from type
-			@load_method = case @type.downcase
-				when "int"
-					%Q(getString("#{@name}", "#{@default}").toInt)
-				when "long"
-					%Q(getString("#{@name}", "#{@default}").toLong)
-				when "float"
-					%Q(getString("#{@name}", "#{@default}").toFloat)
-				when "double"
-					%Q(getString("#{@name}", "#{@default}").toDouble)
-				when "string"
-					%Q(getString("#{@name}", "#{@default}"))
-				when "boolean"
-					%Q(getBoolean("#{@name}", #{@default}))
-				else
-					raise "unknown type `#{@type}`"
-			end
-			if @component == "CheckBoxPreference"
-				@type = "Boolean"
-			end
+      # inspect type from default value
+      unless @type
+        if @default
+          @type = if @default.downcase == "true" || @default.downcase == "false"
+            "Boolean"
+          elsif @default =~ /^-?\d*\.\d+(f|F)?$/
+            "Float"
+          elsif @default =~ /^-?\d+(l)$/
+            "Long"
+          elsif @default =~ /^-?\d+$/
+            "Int"
+          else
+            "String"
+          end
+        end
+      end
+      # default
+      @type ||= "String"
+      
+      # get load_method from type
+      @load_method = case @type.downcase
+        when "int"
+          %Q(getString("#{@name}", "#{@default}").toInt)
+        when "long"
+          %Q(getString("#{@name}", "#{@default}").toLong)
+        when "float"
+          %Q(getString("#{@name}", "#{@default}").toFloat)
+        when "double"
+          %Q(getString("#{@name}", "#{@default}").toDouble)
+        when "string"
+          %Q(getString("#{@name}", "#{@default}"))
+        when "boolean"
+          %Q(getBoolean("#{@name}", #{@default}))
+        else
+          raise "unknown type `#{@type}`"
+      end
+      if @component == "CheckBoxPreference"
+        @type = "Boolean"
+      end
 
-			# recursive call myself
-			@children = []
-			if @component == "PreferenceCategory"
-				@children = e.reject { |child|
-					child.to_s =~ /^\s*$/
-				}.map { |child|
-					Item::new(child)
-				}.compact
-			end
-		end
-	end
+      # recursive call myself
+      @children = []
+      if @component == "PreferenceCategory"
+        @children = e.reject { |child|
+          child.to_s =~ /^\s*$/
+        }.map { |child|
+          Item::new(child)
+        }.compact
+      end
+    end
+  end
 
-	# parse xml file
-	def parse()
-		prefs_content = File::new(@xml_source).read
-		parsed = REXML::Document.new prefs_content
+  # parse xml file
+  def parse()
+    prefs_content = File::new(@xml_source).read
+    parsed = REXML::Document.new prefs_content
 
-		@items = parsed.root.reject { |e|
-			e.to_s =~ /^\s*$/
-		}.map { |e|
-			Item::new(e)
-		}
+    @items = parsed.root.reject { |e|
+      e.to_s =~ /^\s*$/
+    }.map { |e|
+      Item::new(e)
+    }
 
-		@items = @items.map { |e|
-			e.has_child? ? e.children : e
-		}.flatten
+    @items = @items.map { |e|
+      e.has_child? ? e.children : e
+    }.flatten
 
-	end
+  end
 
-	# make scala loader file
+  # make scala loader file
   def make()
-		dist_source = nil
+    dist_source = nil
     open("#{File::dirname(__FILE__)}/#{TEMPLATE_FILE_NAME}") do |data|
-			dist_source = eval(ERB.new(data.read, nil, '-').src, binding, __FILE__, __LINE__)
-		end
+      dist_source = eval(ERB.new(data.read, nil, '-').src, binding, __FILE__, __LINE__)
+    end
 
-		if @xml_source =~ /(^.+\\main\\)/		# mvn style
-			base_dir = $1
-			dist_dir = @namespace.gsub(/\./, "\\")
-			output_folder = "#{base_dir}scala\\#{dist_dir}"
-		elsif @xml_source =~ /^(.+\\)res/		# ant style
-			base_dir = $1
-			dist_dir = @namespace.gsub(/\./, "\\")
-			output_folder = "#{base_dir}src\\#{dist_dir}"
-		else
-			raise "faild to get base directory" 
-		end
-		output_path = "#{output_folder}\\#{@file_name}"
-		Dir::mkdir(output_folder) unless File::exists?(output_folder)
+    if @xml_source =~ /(^.+\\main\\)/   # mvn style
+      base_dir = $1
+      dist_dir = @namespace.gsub(/\./, "\\")
+      output_folder = "#{base_dir}scala\\#{dist_dir}"
+    elsif @xml_source =~ /^(.+\\)res/   # ant style
+      base_dir = $1
+      dist_dir = @namespace.gsub(/\./, "\\")
+      output_folder = "#{base_dir}src\\#{dist_dir}"
+    else
+      raise "faild to get base directory" 
+    end
+    output_path = "#{output_folder}\\#{@file_name}"
+    Dir::mkdir(output_folder) unless File::exists?(output_folder)
 
 
-		File::open(output_path, 'w') do |file|
-			file.write(dist_source)
-		end
-		puts "success! output file > #{output_path}"
-	end
+    File::open(output_path, 'w') do |file|
+      file.write(dist_source)
+    end
+    puts "success! output file > #{output_path}"
+  end
 
 end
 
